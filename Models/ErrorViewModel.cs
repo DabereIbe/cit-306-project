a1b4f162f6bdc7267c1ed0e20466f0b0daf65c9e
DabereIbe <dibeakanma@gmail.com>
2023-07-12 22:03:15 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cit_306_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
